271ff4282295dcd12512144c5922473722bb821e
Michael <michaelsuiyangqiu@gmail.com>
2025-01-22 11:12:29 +1100

    Added in temporary template

---
+++ myapp/src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh", 
+      fontSize: "3rem" 
+    }}>
+      Welcome to my website (only a template for now lol)
+    </div>
+  );
+};
+
+export default Home;
